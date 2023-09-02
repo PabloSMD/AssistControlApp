02714b75c5fade1f6c0cdec54ee03a2df63b3497
PabloSMD <pabloasmdz3@hotmail.com>
2023-09-01 23:29:59 -0400

    AssistControlApp-Back

---
+++ AssistControlApp-back/src/test/java/com/example/AssistControlAppback/AssistControlAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AssistControlAppback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistControlAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
